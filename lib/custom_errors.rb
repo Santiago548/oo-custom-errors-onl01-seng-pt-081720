14d36410e53e9212e5959f571d2531d1caf1893a
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-19 23:19:44 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,7 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.claa != Person
+    if person.class != Person
       raise PartnerError
     else
       person.partner = self
